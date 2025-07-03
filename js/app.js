f057b10d1bdac21a4720faa821833b693a23e49f
Axel <axalberto250@gmail.com>
2025-07-03 14:05:39 -0600

    prueba

--- js/app.js
+++ js/app.js
@@ -66,7 +66,6 @@ const excels = {
     "Newton Raphson": "assets/excels/newton_raph.png",
     "Secante": "assets/excels/secante.png",
     "Punto fijo": "assets/excels/punto_fijo.png",
-    "Montante": "assets/excels/montante.png",
     "Interpolación y Método de Lagrange": "assets/excels/lagrange.png",
     "Regresión lineal y regresión lineal múltiple": "assets/excels/regresion.png",
     "Regresión polinomial": "assets/excels/regresion_polinomial.png",
